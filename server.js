8ae50260a85232fd7989034388d081733f6fe574
nwodzis2 <60149019+nwodzis2@users.noreply.github.com>
2021-09-24 00:03:36 -0400

    restructuring and scripts for heroku

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use(require("./routes/record"));
 
 
 const dbo = require("./db/conn");
+// Accessing the path module
+const path = require("path");
 
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 app.listen(port, () => {
     dbo.connectToServer(function(err){
         if(err) console.error(err);
