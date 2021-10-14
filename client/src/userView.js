44f1cc5c81d66ebcd4c85074d28872a8008f4af1
dcaudill724 <dustincaudill724@gmail.com>
2021-10-13 21:25:57 -0400

    Added eventCard

--- client/src/userView.js
+++ client/src/userView.js
@@ -5,8 +5,29 @@ import { Container, Row, Col} from 'react-bootstrap';
 import { BrowserRouter as Router,
   Switch, Route, Link} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+var eventCard;
 
 class UserView extends React.Component{
+  
+  componentWillMount() {
+     //axios.get("http://localhost:5000/users/614d06e8aae5da24fff0e35e");
+     
+      eventCard = (
+        <div class="eventCard">
+          <div class="eventCardImage"></div>
+          <div class="eventCardName">Test Name</div>
+          <div class="eventCardTime">10:00pm - 12:00pm</div>
+          <div class="eventCardDate">10/13/2021</div>
+        </div>
+      );
+  }
+
+  componentDidMount() {
+    ReactDOM.render(eventCard, document.getElementsByClassName('eventDisplay')[0]);
+  }
+
   render(){
     return(
       <Container fluid>
@@ -17,8 +38,8 @@ class UserView extends React.Component{
         </Row>
         <Row>
               <h1 className="eventTitle">By School</h1>
-              <div className="eventDisplay">
-
+              <div id="testEvent" className="eventDisplay">
+                  
                 </div>
         </Row>
         <Row>
