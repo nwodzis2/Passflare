c315cbc164a96ea7c65bd77f3f84cc4eecd4946c
nwodzis2 <60149019+nwodzis2@users.noreply.github.com>
2021-11-04 13:11:45 -0400

    disable pinch to zoom

--- client/src/index.js
+++ client/src/index.js
@@ -26,7 +26,7 @@ import { BrowserRouter as Router,
         <Helmet>
           <meta charset="utf-8"/>
           <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           <title>Passflare Login</title>
           <meta name="description" content=""/>
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"/>        
