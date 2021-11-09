965ce6418892e123c5d84b034b24609af5449284
nwodzis2 <60149019+nwodzis2@users.noreply.github.com>
2021-11-09 02:04:00 -0500

    Update userLogin.js
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update userLogin.js
    
    Update userLogin.js

--- client/src/index.js
+++ client/src/index.js
@@ -36,24 +36,26 @@ import { BrowserRouter as Router,
           <link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'/>
           <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/>
+          <meta name="apple-mobile-web-app-title" content="Passflare"/>
+          <meta name="apple-mobile-web-app-capable" content="yes"/>
           <link rel="stylesheet" href="styles.css"></link>
           {/* iPhone X (1125px x 2436px) */}
-          <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="passflareLogo.png"></link>
+          <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="/passflareLogo.png"></link>
           {/* iPhone 8, 7, 6s, 6 (750px x 1334px) */}
           <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="/passflareLogo.png"></link>
           {/* iPhone 8 Plus, 7 Plus, 6s Plus, 6 Plus (1242px x 2208px) */}
-          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" href="passflareLogo.png"></link>
+          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" href="/passflareLogo.png"></link>
           {/* iPhone 5 (640px x 1136px) */}
-          <link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" href="passflareLogo.png"></link>
+          <link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" href="/passflareLogo.png"></link>
           {/* iPad Mini, Air (1536px x 2048px) */}
-          <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="passflareLogo.png"></link>
+          <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="/passflareLogo.png"></link>
           {/* iPad Pro 10.5" (1668px x 2224px) */}
-          <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" href="passflareLogo.png"></link>
+          <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" href="/passflareLogo.png"></link>
           {/* iPad Pro 12.9" (2048px x 2732px) */}
-          <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="passflareLogo.png"></link>
+          <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="/passflareLogo.png"></link>
           {/*icon for ios*/}
-          <link rel="apple-touch-icon" sizes="180x180" href="passflareLogo.png"></link>
-          <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+          <link rel="apple-touch-icon" sizes="180x180" href="/passflareLogo.png"></link>
+          
         </Helmet>
       );
     }
